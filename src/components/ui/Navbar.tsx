4f4c91cd026ca7bc3e724c6b706bc5e8d22d9bb1
Josue Diaz <diazfloresj100@gmail.com>
2025-10-25 22:12:25 -0700

    Added new resume link in Navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
             Home
           </Link>
           <Link
-            href="https://docs.google.com/document/d/1qOoDgDwUmOAClHW3tY40I3C1_TTFSWpNk2J_5Rku9Q8/edit?usp=sharing"
+            href="https://docs.google.com/document/d/15er8_WzTZJRfiNEdJ1OGnU94pvwT9bbR/edit?usp=sharing&ouid=104281012583922667065&rtpof=true&sd=true"
             className="relative inline-block
              after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0
              after:bg-current after:transition-all after:duration-300
